8eb225509e7c472377f1a77d8099d83dc98c9432
marcos paulo machado azevedo <marcospaulomachadoazevedo@MacBook-Air-de-marcos.local>
2025-10-18 20:09:06 -0300

    permitindo warnings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      onwarn(_warning, _warn) {
+        // Ignora todos os warnings durante o build
+        return;
+      }
+    }
+  }
 })
